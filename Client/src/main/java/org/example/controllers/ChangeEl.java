856f4951789b962023a3e19d091461d5f17de7cb
blizzard <blizzard5002@yandex.ru>
2024-06-14 13:36:11 +0300

    neXt

--- Client/src/main/java/org/example/controllers/ChangeEl.java
+++ Client/src/main/java/org/example/controllers/ChangeEl.java
@@ -1,5 +1,55 @@
 package org.example.controllers;
 
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class ChangeEl {
     //Добавить логику управления изменения и добавления объекта
+    @FXML
+    private TextField F_key;
+    @FXML
+    private TextField F_p_name;
+    @FXML
+    private TextField F_gr_name;
+    @FXML
+    private TextField F_p_height;
+    @FXML
+    private TextField F_st_count;
+    @FXML
+    private TextField F_p_weight;
+    @FXML
+    private TextField F_p_color;
+    @FXML
+    private TextField F_shouldExp;
+    @FXML
+    private TextField F_form;
+    @FXML
+    private TextField F_sem;
+    @FXML
+    private TextField F_l_x;
+    @FXML
+    private TextField F_l_y;
+    @FXML
+    private TextField F_c_x;
+    @FXML
+    private TextField F_l_z;
+    @FXML
+    private TextField F_c_y;
+    @FXML
+    private TextField F_l_name;
+    @FXML
+    private Button apply;
+
+    public void insertEl(){
+
+    }
+    public void updateEl(){
+
+    }
+    public void deleteEl(){
+
+    }
+
 }
--- Client/src/main/java/org/example/controllers/MainScene.java
+++ Client/src/main/java/org/example/controllers/MainScene.java
@@ -1,17 +1,34 @@
 package org.example.controllers;
 
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.TableColumn;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.example.island.object.TableGroup;
 import org.example.island.object.TableGroup;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 
 public class MainScene {
+    @FXML
+    private AnchorPane anchor;
     @FXML
     private TableView<TableGroup> table;
     @FXML
@@ -53,11 +70,68 @@ public class MainScene {
     @FXML
     private TableColumn<TableGroup, String> owner;
 
-
+    public void initialize() {
+        table.setRowFactory(tv -> {
+            TableRow<TableGroup> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (!row.isEmpty())) {
+                    try {
+                        List<Node> anchorPanesToRemove = new ArrayList<>();
+                        for (Node child : anchor.getChildren()) {
+                            if (child instanceof AnchorPane) {
+                                anchorPanesToRemove.add(child);
+                            }
+                        }
+                        anchor.getChildren().removeAll(anchorPanesToRemove);
+                        FXMLLoader loader = new FXMLLoader();
+                        loader.setLocation(getClass().getResource("/elSetting.fxml"));
+                        Parent vboxMenu = loader.load();
+                        vboxMenu.setLayoutX(event.getSceneX());
+                        vboxMenu.setLayoutY(event.getSceneY());
+                        anchor.getChildren().add(vboxMenu);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            return row;
+        });
+    }
+//    public void v(Parent vboxMenu){
+//        EventHandler<MouseEvent> closeHandler = new EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent e) {
+//                // Проверяем, что клик был вне vboxMenu
+//                if (vboxMenu != null && !vboxMenu.contains(e.getSceneX() - vboxMenu.getLayoutX(), e.getSceneY() - vboxMenu.getLayoutY())) {
+//                    anchor.getChildren().remove(vboxMenu);
+//                    anchor.removeEventHandler(MouseEvent.MOUSE_CLICKED, this);
+//                }
+//            }
+//        };
+//
+//        anchor.addEventHandler(MouseEvent.MOUSE_CLICKED, closeHandler);
+//    }
+//    public void showAct(){
+//        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/elSetting.fxml"));
+//        Parent root = null;
+//        try {
+//            root = fxmlLoader.load();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        Scene scene = new Scene(root);
+//
+//        Stage popupStage = new Stage();
+//        popupStage.setScene(scene);
+//        popupStage.showAndWait();
+//    }
     public void collectionInit(List<TableGroup> data){
-        ObservableList<TableGroup> list = FXCollections.observableList(data);
-        list.addAll(); // определить логику для команд, нажимаешь на строку таблицы - окно с действиями над объектом, остальные команды можно в меню скинуть
+        ObservableList<TableGroup> list = FXCollections.observableList(data);// определить логику для команд, нажимаешь на строку таблицы - окно с действиями над объектом, остальные команды можно в меню скинуть
         table.setItems(list);
+    }
+    public void rowSelected(){
+        System.out.println("ehhhff");
     }
 
 }
