b1801716d310ebcd653b01f65dacc35c3dd684d6
blizzard <blizzard5002@yandex.ru>
2024-06-15 16:17:22 +0300

    mark

---
+++ Client/src/main/java/org/example/controllers/LangControl.java
@@ -0,0 +1,42 @@
+package org.example.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class LangControl {
+    @FXML
+    private Button Brus;
+    @FXML
+    private Button Bengl;
+    @FXML
+    private Button Beest;
+    @FXML
+    private Button Blatw;
+    private MainScene mainScene;
+
+    public void rus(){
+        mainScene.setLocale(new Locale("ru", "Rus"));
+    }
+    public void init(MainScene mainScene){
+        this.mainScene = mainScene;
+    }
+    public void latw(){
+    }
+    public void eest(){
+
+    }
+    public void engl(){
+        mainScene.setLocale(new Locale("en", "Us"));
+    }
+
+//    public void setLocale(Locale locale) {
+//        ResourceBundle bundle = ResourceBundle.getBundle("MessagesBundle", locale);
+//        // Обновление текста элементов интерфейса
+//        myLabel.setText(bundle.getString("myLabel.text"));
+//        // ... обновление других элементов
+//    }
+
+}
