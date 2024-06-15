417eaad550b4b6b52d57c4e25777e9d07adf7edf
p11menova <p11menova@yandex.ru>
2024-06-15 15:53:22 +0300

    fixed

--- Client/src/main/java/org/example/controllers/InsertControl.java
+++ Client/src/main/java/org/example/controllers/InsertControl.java
@@ -5,9 +5,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.example.island.commands.InsertNull;
 import org.example.island.object.*;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class InsertControl {
     @FXML
@@ -44,6 +47,8 @@ public class InsertControl {
     private TextField F_l_name;
     @FXML
     private Button insert;
+    @FXML
+    private Text error_message;
     private MainScene mainScene;
 
     public void init(MainScene mainScene){
@@ -54,32 +59,42 @@ public class InsertControl {
 
     }
     public void insert(){
-        Coordinates coordinates = new Coordinates(Float.parseFloat(F_c_x.getText()), Double.parseDouble(F_c_y.getText()));
-        Location location = new Location(
-                Long.parseLong(F_l_x.getText()),
-                Long.parseLong(F_l_y.getText()),
-                Integer.parseInt(F_l_z.getText()),
-                F_l_name.getText());
-        Person person = new Person(
-                F_p_name.getText(),
-                Float.parseFloat(F_p_height.getText()),
-                Double.parseDouble(F_p_weight.getText()),
-                Color.valueOf(F_p_color.getValue().toString()),
-                location);
-        StudyGroup studyGroup = new StudyGroup(
-                F_gr_name.getText(),
-                Long.parseLong(F_st_count.getText()),
-                Long.parseLong(F_shouldExp.getText()),
-                coordinates,
-                FormOfEducation.valueOf(F_form.getValue().toString()),
-                Semester.valueOf( F_sem.getValue().toString()),
-                person);
-        Object[] args = new Object[2];
-        args[0] = Integer.parseInt(F_key.getText());
-        args[1] = studyGroup;
-        InsertNull insertNull = new InsertNull();
-        insertNull.setArguments(args);
-        mainScene.process(insertNull);
+        AtomicBoolean ok = new AtomicBoolean(false);
+        try {
+            Coordinates coordinates = new Coordinates(Float.parseFloat(F_c_x.getText()), Double.parseDouble(F_c_y.getText()));
+            Location location = new Location(
+                    Long.parseLong(F_l_x.getText()),
+                    Long.parseLong(F_l_y.getText()),
+                    Integer.parseInt(F_l_z.getText()),
+                    F_l_name.getText());
+            Person person = new Person(
+                    F_p_name.getText(),
+                    Float.parseFloat(F_p_height.getText()),
+                    Double.parseDouble(F_p_weight.getText()),
+                    Color.valueOf(F_p_color.getValue().toString()),
+                    location);
+            StudyGroup studyGroup = new StudyGroup(
+                    F_gr_name.getText(),
+                    Long.parseLong(F_st_count.getText()),
+                    Long.parseLong(F_shouldExp.getText()),
+                    coordinates,
+                    FormOfEducation.valueOf(F_form.getValue().toString()),
+                    Semester.valueOf(F_sem.getValue().toString()),
+                    person);
+            Object[] args = new Object[2];
+            args[0] = Integer.parseInt(F_key.getText());
+            args[1] = studyGroup;
+            InsertNull insertNull = new InsertNull();
+            insertNull.setArguments(args);
+            mainScene.process(insertNull);
+
+            ok.set(true);
+
+        } catch(Exception e){
+            error_message.setText("введены невалидные данные( повторите попытку");
+            Stage st = (Stage) insert.getScene().getWindow();
+            st.showAndWait();
+        }
         Stage st = (Stage) insert.getScene().getWindow();
         st.close();
     }
